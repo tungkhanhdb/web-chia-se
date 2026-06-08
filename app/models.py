05302df9b47e0d631f802a6f4be16338e474b7d6
noob7-e <noob102007@gmail.com>
2026-06-08 11:16:48 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,10 +11,9 @@ class User(db.Model, UserMixin):
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False) # Tên file khi lưu trên server
-    original_name = db.Column(db.String(255), nullable=False) # Tên file gốc người dùng đặt
+    filename = db.Column(db.String(255), nullable=False) 
+    original_name = db.Column(db.String(255), nullable=False)
     file_type = db.Column(db.String(50)) # Ví dụ: PDF, DOCX
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
-    # Quan hệ ngược: Để sau này khi muốn xem user đó đăng những file nào
     owner = db.relationship('User', backref=db.backref('documents', lazy=True))
